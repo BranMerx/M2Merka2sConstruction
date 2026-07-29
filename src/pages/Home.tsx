fd51d2327122d6e00ea10cb54051008da6d42e5a
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-07-29 19:56:50 -0400

    Adding Company Logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import logo from '../assets/logo/M2 Merka2s Construction Logo.png'
+
 function Home() {
     return (
         <div>
+            <img src={logo} alt="Company Logo" width="200"/>
             <h1>Welcome to M2 Merka2s Construction</h1>
             <p>Built On Trust. Built By Family. Built To Last.</p>
             <p>A Family Business You can Trust.</p>
