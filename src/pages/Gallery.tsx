6c3b35c9656f8c2ecc647b11e00a5a73e46a981f
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-07-29 13:00:43 -0400

    initial commit

---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            Gallery Page
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Referral.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            Referral Page
+        </div>
+    );
+}
+
+export default Home;
