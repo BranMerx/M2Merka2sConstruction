2d9792f4b53c52a9ce6496daae98cc746606afb7
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-06 16:14:05 -0400

    Editing the about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,14 @@ function About() {
     return (
         <div>
             <h1>This is Us:</h1>
-            <p>Founded by our owner Arturo Mercado-Cancino, we are a carpentry business that is dedicated to providing you with...</p>
+            <p>We are a family-run and founded carpentry business located in the village of Romeo, Michigan. We are
+            ecstatic to meet the communities that surround us and help you in any of our carpentry endeavors. 
+            We have many years of experience in a variety of areas such as drywall, showers, painting, framing,
+            and others just to name a few. Even if we can not help you with a specific project, we for sure can
+            help you find someone who can!</p>
+            <p>Whether you are looking to start a project, need help finishing one, or are just shopping around for,
+            we want to help. We are located in Macomb County and are eager to assist our customers located in the 
+            surrounding areas. We are a registered and insured company. Please do not hesitate to reach out!</p>
         </div>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,11 @@ function Home() {
                 style={{border: '4px solid #ebca25', borderRadius:'8px'}}
                 width="200"/>
             <h1>Welcome to M2 Merka2s Construction</h1>
-            <p>Built On Trust. Built By Family. Built To Last.</p>
-            <p>A Family Business You can Trust.</p>
+            <h2>Built On Trust. Built By Family. Built To Last.</h2>
+            <h2>A Family Business You can Trust.</h2>
+            <h3>Contact Information:</h3>
+            <h3>586-894-3206</h3>
+            <h3>lobitoman1@live.com</h3>
         </div>
     );
 }
