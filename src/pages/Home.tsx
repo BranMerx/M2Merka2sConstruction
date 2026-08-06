d3209d189c4be1f1abdb1f5d328047f8d385b5e6
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-06 15:48:14 -0400

    Adding border to logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,10 @@ import logo from '../assets/logo/M2 Merka2s Construction Logo.png'
 function Home() {
     return (
         <div>
-            <img src={logo} alt="Company Logo" width="200"/>
+            <img src={logo} 
+                alt="Company Logo" 
+                style={{border: '4px solid #ebca25', borderRadius:'8px'}}
+                width="200"/>
             <h1>Welcome to M2 Merka2s Construction</h1>
             <p>Built On Trust. Built By Family. Built To Last.</p>
             <p>A Family Business You can Trust.</p>
