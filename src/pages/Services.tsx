005d8fa62561090aa2f3bcbfa37a4641b4f66f68
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-07 14:24:25 -0400

    Services list is updated

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,11 +1,16 @@
+import "../components/ServiceCard/Services.css"
 function Services() {
     const services = ["Drywall", "Tile", "Showers", "Painting", "Framing", "Basement Finish", "Outdoor Patios", "Pergolas"];
     return (
-        <ul>
-            {services.map((service, index) => (
-                <li key = {index}>{service}</li>
-            ))}
-        </ul>
+        <div>
+            <h1>These are some common services we provide:</h1>
+            <ul className="list-services">
+                {services.map((service, index) => (
+                    <li key = {index}>{service}</li>
+                ))}
+            </ul>
+            <h2>These services and many more!</h2>
+        </div>
     );
 }
 
