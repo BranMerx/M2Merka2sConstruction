f3453dbfc30057d16c0c37a6566a6f280f8c1f2d
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-07 17:20:20 -0400

    Logo addition

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,9 +1,14 @@
 import "../components/ServiceCard/Services.css"
+import logo from "../assets/logo/M2 Merka2s Construction Logo.png"
 function Services() {
     const services = ["Drywall", "Tile", "Showers", "Painting", "Framing", "Basement Finish", "Outdoor Patios", "Pergolas"];
     return (
         <div>
             <h1>List of Common Services Provided:</h1>
+            <img src={logo} 
+                alt="Company Logo" 
+                style={{border: '4px solid #d4af37', borderRadius:'8px'}}
+                width="400"/>
             <ul className="list-services">
                 {services.map((service, index) => (
                     <li key = {index}>{service}</li>
