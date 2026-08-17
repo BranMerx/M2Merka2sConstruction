1110b0a95aa792a43c07a7f8280a7b160de13015
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-16 20:18:18 -0400

    adding more images to gallery

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -7,6 +7,24 @@ import stairs from "../assets/gallery/Stairs.jpeg"
 import wall from "../assets/gallery/Wall.jpeg"
 import fireplace from "../assets/gallery/Fireplace.jpg"
 import pillars from "../assets/gallery/Pillars.jpg"
+import bathroom from "../assets/gallery/Bathroom.jpeg"
+import chimney_brick from "../assets/gallery/Chimney_Brick.jpeg"
+import chimney_arch from "../assets/gallery/Chimney_with_arch.jpeg"
+import deck from "../assets/gallery/Deck.jpeg"
+import second_deck from "../assets/gallery/Deck_2.jpeg"
+import deck_with_railing from "../assets/gallery/Deck_and_Railing.jpeg"
+import downspouts from "../assets/gallery/Downspouts.jpeg"
+import fireplace_install from "../assets/gallery/Fireplace_Installation.jpeg"
+import garage_flooring from "../assets/gallery/Garage_Flooring.jpeg"
+import kitchen_cabinets from "../assets/gallery/Kitchen_Cabinets.jpeg"
+import outdoor from "../assets/gallery/Outdoor Trimming and Mulch Maintenance.jpeg"
+import glass_railing from "../assets/gallery/Paving_and_Glass_Railing.jpeg"
+import peragola from "../assets/gallery/Peragola.jpeg"
+import bathroom_shower_toilette from "../assets/gallery/Shower_Tile_Toilette.jpeg"
+import white_shower from "../assets/gallery/Shower_White.jpeg"
+import staircase_carpet from "../assets/gallery/Staircase_Carpet.jpeg"
+import stone_veneer from "../assets/gallery/Stone_Veneer.jpeg"
+import arch from "../assets/gallery/Upclose_Arch.jpeg"
 
 export const galleryImages = [
     {
@@ -53,5 +71,110 @@ export const galleryImages = [
         id: 9,
         title:"Pillars",
         image: pillars,
+    },
+        {
+        id: 10,
+        title:"Finished Bathroom",
+        image: bathroom,
+    },
+        {
+        id: 11,
+        title:"Brick Veneer for Chimney",
+        image: chimney_brick,
+    },
+        {
+        id: 12,
+        title:"Chimney with Arch",
+        image: chimney_arch,
+    },
+        {
+        id: 13,
+        title:"Finished Bathroom",
+        image: bathroom,
+    },
+        {
+        id: 14,
+        title:"Brick Veneer for Chimney",
+        image: chimney_brick,
+    },
+        {
+        id: 15,
+        title:"Outdoor Deck",
+        image: deck,
+    },
+        {
+        id: 16,
+        title:"Deck",
+        image: second_deck,
+    },
+        {
+        id: 17,
+        title:"Deck with Railing",
+        image: deck_with_railing,
+    },
+        {
+        id: 18,
+        title:"Chimney with Arch",
+        image: chimney_arch,
+    },
+        {
+        id: 19,
+        title:"Downspouts",
+        image: downspouts,
+    },
+        {
+        id: 20,
+        title:"Fireplace Installation",
+        image: fireplace_install,
+    },
+        {
+        id: 21,
+        title:"Garage Flooring",
+        image: garage_flooring,
+    },
+        {
+        id: 22,
+        title:"Kitchen Cabinets",
+        image: kitchen_cabinets,
+    },
+        {
+        id: 23,
+        title:"Outdoor Maintenance",
+        image: outdoor,
+    },
+        {
+        id: 24,
+        title:"Glass Railing",
+        image: glass_railing,
+    },
+        {
+        id: 25,
+        title:"Peragola",
+        image: peragola,
+    },
+        {
+        id: 26,
+        title:"Full Bathroom",
+        image: bathroom_shower_toilette,
+    },
+        {
+        id: 27,
+        title:"Shower Updates",
+        image: white_shower,
+    },
+        {
+        id: 28,
+        title:"Staircase with Carpet",
+        image: staircase_carpet,
+    },
+        {
+        id: 29,
+        title:"Stone Veneer",
+        image: stone_veneer,
+    },
+        {
+        id: 30,
+        title:"Arches",
+        image: arch,
     },
 ];
