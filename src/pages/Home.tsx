3e208e07e04867e6938c68afd9ccaf9285b2ca7e
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-19 14:34:58 -0400

    Seprating Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
-import logo from '../assets/logo/M2 Merka2s Construction Logo.png'
+import logo from '../assets/logo/M2 Merka2s Construction Logo.png';
+import "../styles/Home.css";
 
 function Home() {
     return (
-        <div>
+        <div className="home-container">
             <h1>Welcome to M2 Merka2s Construction</h1>
-            <img src={logo} 
-                alt="Company Logo" 
-                style={{border: '4px solid #d4af37', borderRadius:'8px'}}
-                width="400"/>
+
+            <img
+                src={logo}
+                alt="Company Logo"
+                className="home-logo"
+            />
+
             <h2>Built On Trust. Built By Family. Built To Last.</h2>
-            <h2>A Family Business You can Trust.</h2>
+            <h2>A Family Business You Can Trust.</h2>
+
             <h3>Contact Information:</h3>
             <h3>586-894-3206</h3>
             <h3>lobitoman1@live.com</h3>
