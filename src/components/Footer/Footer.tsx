dbf07c8b4fc5f96abacf5869ffe210d0709b35ce
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-19 15:01:59 -0400

    Update to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
+import "../Footer/Footer.css"
 function Footer(){
     return(
         <footer>
-            <p>© 2026 Merka2s Construction</p>
-            <p>Website developed and maintained by Brandon Mercado Franco</p>
+            <div className="copyright-text">
+                <p>© 2026 M2 Merka2s Construction</p>
+                <p>Website developed and maintained by Brandon Mercado Franco</p>
+            </div>
         </footer>
     );
 }
