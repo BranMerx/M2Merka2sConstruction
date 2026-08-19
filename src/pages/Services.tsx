7fbddbae9eef4f9cc3d255b2c0076efc6c28d229
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-08-19 16:40:56 -0400

    Updating the services lists

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,19 +1,57 @@
-import "../components/ServiceCard/Services.css"
-import logo from "../assets/logo/M2 Merka2s Construction Logo.png"
+import "../components/ServiceCard/Services.css";
+import logo from "../assets/logo/M2 Merka2s Construction Logo.png";
+
 function Services() {
-    const services = ["Drywall", "Tile", "Showers", "Painting", "Framing", "Basement Finish", "Outdoor Patios", "Pergolas"];
+    const indoor_services = [
+        "Attic Remodels",
+        "Basements Remodels",
+        "Countertops",
+        "Pillars",
+        "Drywall",
+        "Tile",
+        "Shower Remodels"
+    ];
+
+    const outdoor_services = [
+        "Outdoor Patios",
+        "Pergolas",
+        "Downspouts",
+        "Mulching",
+        "Stone Veneer",
+        "Railing",
+        "Fencing"
+    ];
+
     return (
-        <div>
+        <div className="services-container">
             <h1>List of Common Services Provided:</h1>
-            <img src={logo} 
-                alt="Company Logo" 
-                style={{border: '4px solid #d4af37', borderRadius:'8px'}}
-                width="400"/>
-            <ul className="list-services">
-                {services.map((service, index) => (
-                    <li key = {index}>{service}</li>
-                ))}
-            </ul>
+
+            <img
+                src={logo}
+                alt="Company Logo"
+                className="services-logo"
+            />
+
+            <div className="services-lists">
+                <div className="service-list">
+                    <h2>Indoor Services</h2>
+                    <ul>
+                        {indoor_services.map((service, index) => (
+                            <li key={index}>{service}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="service-list">
+                    <h2>Outdoor Services</h2>
+                    <ul>
+                        {outdoor_services.map((service, index) => (
+                            <li key={index}>{service}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
             <h2>These services and many more!</h2>
         </div>
     );
